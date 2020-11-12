674e4a46a5119de4a97d338cd06c2e9eb0201d70
andreyklimenko <andrey.iemail@gmail.com>
2020-11-12 22:20:26 +0200

    Add EasyAdmin pack

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -18,73 +18,78 @@ class Conference
      * @ORM\Column(type="integer")
      */
     private $id;
-
+    
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $city;
-
+    
     /**
      * @ORM\Column(type="string", length=4)
      */
     private $year;
-
+    
     /**
      * @ORM\Column(type="boolean")
      */
     private $isInternational;
-
+    
     /**
      * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="conference", orphanRemoval=true)
      */
     private $comments;
-
+    
     public function __construct()
     {
         $this->comments = new ArrayCollection();
     }
-
+    
+    public function __toString(): string
+    {
+        return (string)$this->getCity() . ' ' . (string)$this->getYear();
+    }
+    
     public function getId(): ?int
     {
         return $this->id;
     }
-
+    
     public function getCity(): ?string
     {
         return $this->city;
     }
-
+    
     public function setCity(string $city): self
     {
         $this->city = $city;
-
+        
         return $this;
     }
-
+    
     public function getYear(): ?string
     {
         return $this->year;
     }
-
+    
     public function setYear(string $year): self
     {
         $this->year = $year;
-
+        
         return $this;
     }
-
+    
     public function getIsInternational(): ?bool
     {
         return $this->isInternational;
     }
-
+    
     public function setIsInternational(bool $isInternational): self
     {
         $this->isInternational = $isInternational;
-
+        
         return $this;
     }
-
+    
     /**
      * @return Collection|Comment[]
      */
@@ -92,17 +97,17 @@ class Conference
     {
         return $this->comments;
     }
-
+    
     public function addComment(Comment $comment): self
     {
         if (!$this->comments->contains($comment)) {
             $this->comments[] = $comment;
             $comment->setConference($this);
         }
-
+        
         return $this;
     }
-
+    
     public function removeComment(Comment $comment): self
     {
         if ($this->comments->removeElement($comment)) {
@@ -111,7 +116,7 @@ class Conference
                 $comment->setConference(null);
             }
         }
-
+        
         return $this;
     }
 }
